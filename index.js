4d3cde1c3bfd0fc2ae01894720ec87ba937352ff
maniyarmohammedsaqlain <85744349+maniyarmohammedsaqlain@users.noreply.github.com>
2022-11-18 16:36:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ butt.addEventListener("click",respond)
 var answers={
     "HELLO":"Hello Geek! Nice To meet you",
     "HI":"Hello Geek! Hope you and your loved ones are doing well!May your day becomes better and better!",
-    "WHO ARE YOU":"I am Hytro A Virtual Assistant 😀!How May I help you?",
-    "WHO AM I":"You are One of My favourite User❤",
-    "OK BYE":"Thank you Geek! Will meet you soon!",
+    "WHOAREYOU":"I am Hytro A Virtual Assistant 😀!How May I help you?",
+    "WHOAMI":"You are One of My favourite User❤",
+    "OKBYE":"Thank you Geek! Will meet you soon!",
     "BYE":"Thank you Geek! Will meet you soon!",
     "OK":"😀",
-    "WHAT IS MY NAME":"I  don't Know But I was said to call you GEEK😀",
-    "MY NAME":"I  don't Know But I was said to call you GEEK😀",
-    "How are you":"Well! I am doing very well ! Hope the same from you too😀"
+    "WHATISMYNAME":"I  don't Know But I was said to call you GEEK😀",
+    "MYNAME":"I  don't Know But I was said to call you GEEK😀",
+    "HOWAREYOU":"Well! I am doing very well ! Hope the same from you too😀"
 }
-function respond(event){
+function respond(){
     var get=input.value
     var convert=get.toUpperCase()
-    if ((convert) in answers){
-        givenanwer.innerHTML=answers[convert]
+    var main=convert.replaceAll(" ","")
+    console.log(main);
+    if ((main) in answers){
+        givenanwer.innerHTML=answers[main]
     }
     else{
-        givenanwer.innerHTML="Sorry! I didn't Understand"
+        givenanwer.innerHTML="Sorry!I didn't Understand"
     }
 }
